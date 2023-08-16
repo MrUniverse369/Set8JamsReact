5c69fe8cb4b9fa286460c54cc96d3fd20a0ba55e
MrUniverse369 <132138248+MrUniverse369@users.noreply.github.com>
2023-08-16 19:54:04 +0100

    react ver of set8jamsweb

---
+++ src/Nav.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+function NavBar(){
+    return(<nav
+        className ="navbar navbar-dark bg-dark NavBarContainer navbar-expand-sm h4 border-1 border-bottom ps-0 pe-0 mb-0 ms-0 me-0"
+      >
+        <div className ="container-fluid">
+          <a href="index.html" className="navbar-brand text-white">SET8JAMS</a>
+          <button
+            className ="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#main-nav"
+            aria-controls="main-nav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className ="navbar-toggler-icon"></span>
+          </button>
+  
+          <div className ="collapse navbar-collapse" id="main-nav">
+            <ul className ="navbar-nav">
+              <li className ="nav-item">
+                <a className ="nav-link text-white" href="index.html">Home</a>
+              </li>
+              <li className ="nav-item">
+                <a className ="nav-link text-white" href="TrueNature.html"
+                  >TrueNature</a>
+              </li>
+              <li className ="nav-item">
+                <a className ="nav-link text-white" href="GalacticCrusade.html"
+                  >GalacticCrusade</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>)
+}
+
+export default NavBar;
